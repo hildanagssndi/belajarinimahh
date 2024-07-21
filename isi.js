2dc103353252f0f595748b72827edf7c69c06f37
hildanagssndi <hildanagussandi16@gmail.com>
2024-07-21 19:49:08 +0700

    first commit

---
+++ isi.js
@@ -0,0 +1,7 @@
+document.writeln("<CENTER>");
+document.writeln("<HR WIDTH=600 COLOR=Black>");
+document.writeln("<H1>Universitas Teknologi Bandung</H1>");
+document.writeln("<H2>Fakultas Teknik Informatika<H2>");
+document.writeln("<H3>Jurusan Teknik Informatika<H3>");
+document.writeln("<HR WIDTH=600 COLOR=Black>");
+document.writeln("</CENTER>");
